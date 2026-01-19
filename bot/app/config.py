4e51b1e7f38398ac8ec7511032d8a9a861641deb
Kapitonov Daniil <62303616+daniilkapitonov@users.noreply.github.com>
2026-01-19 11:51:20 +0300

    Update

--- bot/app/config.py
+++ bot/app/config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "_____")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN_IDS = {int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()}
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://example.com")
 API_BASE_URL = os.getenv("API_BASE_URL", "http://backend:8000")
